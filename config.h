5735485b15b27cd75d1ae13a7350cf13ee755c3d
Daryl Galvez <daryl.galvez@outlook.com>
2021-05-08 18:02:39 +0800

    Add urgent background color patch

--- config.h
+++ config.h
@@ -14,11 +14,13 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_urgborder[]   = "#ff0000";
 static const char col_neon[]        = "#1abc9c";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_neon,  col_neon  },
+	[SchemeUrg]  = { col_gray4, col_neon,  col_urgborder  },
 };
 
 /* tagging */
