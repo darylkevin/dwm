5c5c39c0706f0633c6d8f500cd7a6935f6fd92b8
Daryl Galvez <daryl.galvez@outlook.com>
2021-05-16 14:57:51 +0800

    Add network manager applet

--- config.h
+++ config.h
@@ -12,8 +12,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono Nerd Font:size=10" };
-static const char dmenufont[]       = "JetBrains Mono Nerd Font:size=10";
+static const char *fonts[]          = { "JetBrains Mono Nerd Font:size=8" };
+static const char dmenufont[]       = "JetBrains Mono Nerd Font:size=8";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -34,6 +34,7 @@ static const char *const autostart[] = {
 	"flameshot", NULL,
 	"emacs", "--daemon", NULL,
 	"pa-applet", NULL,
+	"nm-applet", NULL,
     "sh", "-c", "~/.doom.d/exwm/swapesc.sh", NULL,
 	"dunst", "-config", "~/.config/dunst/dunstrc", NULL,
 	NULL /* terminate */
