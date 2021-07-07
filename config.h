37077468250f88b139bdc207c691d0b2e32353c3
Daryl Galvez <daryl.galvez@outlook.com>
2021-07-07 12:11:28 +0800

    Change username home directory

--- config.h
+++ config.h
@@ -30,7 +30,7 @@ static const char *colors[][3]      = {
 
 static const char *const autostart[] = {
 	"slstatus", NULL,
-    "feh", "--bg-scale", "/home/xfdg/Pictures/Wallpapers/ZT6cdC7.jpeg", NULL, "emacs", "--daemon", NULL,
+    "feh", "--bg-scale", "/home/xdgx/Pictures/Wallpapers/ZT6cdC7.jpeg", NULL, "emacs", "--daemon", NULL,
 	"pa-applet", NULL,
 	"picom", "--experimental-backends", NULL,
 	"nm-applet", NULL,
