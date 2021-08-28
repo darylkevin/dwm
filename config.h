d233d23033ff50dec8663713f40768a96b2eef27
Daryl Galvez <daryl.galvez@outlook.com>
2021-08-28 21:46:06 +0800

    Recompile dwm

--- config.h
+++ config.h
@@ -80,8 +80,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_neon, "-sf", col_gray4, NULL };
-/* static const char *termcmd[]  = { "alacritty", NULL }; */
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+/*static const char *termcmd[]  = { "st", NULL };*/
 static const char *roficmd[] = { "rofi", "-show", "drun", "-config", "~/.config/rofi/themes/dt-center.rasi", "-display-drun", "'Run: '", "-drun-display-format", "'{name}'", "-show-icons", NULL };
 static const char *emacscmd[] = { "emacsclient", "-c", "-a", "emacs", NULL };
 static const char *maimsave[] = { "msave.sh", NULL };
